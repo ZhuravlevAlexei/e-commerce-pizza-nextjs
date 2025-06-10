3cbf3a16cf6518e7cddadd36f23014ecbec494c2
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2025-06-10 12:00:36 +0300

    prepare deploy

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/prisma-setup/prisma-client';
-import { getUserSession } from '@/shared/lib/get-user-session';
+// import { getUserSession } from '@/shared/lib/get-user-session';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/shared/constants/auth-options';
 
 export const dynamic = 'force-dynamic'; // ПРИ СбОРКЕ не работает getUserSession()
 // // и тогда пытаемся получить серверную сессию в первых 3х строках
@@ -11,9 +13,13 @@ export const dynamic = 'force-dynamic'; // ПРИ СбОРКЕ не работа
 // можно попробовать СБОРКУ с'force-dynamic' но со старой функцией getUserSession();
 // в dev-режиме все нормально работает с getUserSession(); см.22:43:00+
 
-export async function GET() {
+export async function GET(req: any, res: any) {
+  // export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    const user = await getUserSession();
+    const user = await getServerSession(req, res, authOptions);
+    // export async function GET() {
+    //   try {
+    //     const user = await getUserSession();
 
     if (!user) {
       return NextResponse.json(
@@ -24,7 +30,8 @@ export async function GET() {
 
     const data = await prisma.user.findUnique({
       where: {
-        id: Number(user.id),
+        // id: Number(user.id), //for getUserSession
+        id: Number(user.user.id), //for getServerSession
       },
       select: {
         fullName: true,
